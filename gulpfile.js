40ed43936fa663b03f3ee3732b07f0ba3aa4fea2
Joabe Costa <joabetc@hotmail.com>
2018-01-24 23:21:00 -0200

    Setting file search pattern for imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
     imagemin = require('gulp-imagemin');
 
 gulp.task('build-img', function() {
-    gulp.src('src/img')
+    gulp.src('src/img/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('src/img'));
 });
