59a55f38835abccc0fc3a70053f5c5f9c20c947c
Joabe Costa <joabetc@hotmail.com>
2018-01-24 23:50:01 -0200

    Creating a new task for copying source content in order to preserve it

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp'),
     imagemin = require('gulp-imagemin');
 
+gulp.task('copy', function() {
+    gulp.src('src/**/*')
+        .pipe(gulp.dest('dist'));
+});
+    
 gulp.task('build-img', function() {
-    gulp.src('src/img/**/*')
+    gulp.src('dist/img/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('src/img'));
+        .pipe(gulp.dest('dist/img'));
 });
