a9baa98de46af4d58940871d9dcd5ba24a5b601d
Joabe Costa <joabetc@hotmail.com>
2018-01-24 23:53:10 -0200

    Adding a new task for cleaning dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 var gulp = require('gulp'),
-    imagemin = require('gulp-imagemin');
+    imagemin = require('gulp-imagemin'),
+    clean = require('gulp-clean');
 
 gulp.task('copy', function() {
     gulp.src('src/**/*')
         .pipe(gulp.dest('dist'));
 });
     
+gulp.task('clean', function() {
+    gulp.src('dist')
+        .pipe(clean());
+});
+
 gulp.task('build-img', function() {
     gulp.src('dist/img/**/*')
         .pipe(imagemin())
